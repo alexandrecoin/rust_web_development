be2875b3c876ee7b2fe07174e9b0e9d4106a66a6
Alexandre COIN <alexandre@macbook-pro-de-alexandre.home>
2024-05-18 21:03:21 +0200

    ch3: get_questions handler

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,87 @@
+use serde::Serialize;
+use std::{
+    fmt::Debug,
+    io::{Error, ErrorKind},
+    str::FromStr,
+};
 use tokio;
-use warp::Filter;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
-    tags: Option<Vec<String>>
+    tags: Option<Vec<String>>,
 }
 
 impl Question {
-    fn new(id: &str, title: &str, content: &str, tags: Vec<String>) -> Self {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
         Question {
-            id: QuestionId::from(id),
-            title: String::from(title),
-            content: String::from(content),
-            tags: Some(tags)
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
         }
     }
 }
 
 #[derive(Debug)]
-struct QuestionId(String);
+struct InvalidId;
 
-impl QuestionId {
-    fn from(id: &str) -> Self {
-        QuestionId(String::from(id))
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First question".to_string(),
+        "Content".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(_InvalidId) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
