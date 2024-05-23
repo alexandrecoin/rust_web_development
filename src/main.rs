bebd307afc71dd271e0cd612f9f4361de41ff245
Alexandre COIN <alexandre@macbook-pro-de-alexandre.home>
2024-05-23 06:09:49 +0200

    add query params

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::{
-    fmt::Debug,
-    io::{Error, ErrorKind},
-    str::FromStr,
-};
+use std::fmt::Debug;
 use tokio;
 use warp::{
     cors::CorsForbidden,
     http::{Method, StatusCode},
-    reject::Reject,
     Filter, Rejection, Reply,
 };
 
@@ -21,35 +16,8 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-impl Question {
-    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Question {
-            id,
-            title,
-            content,
-            tags,
-        }
-    }
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, Hash, PartialEq)]
 struct QuestionId(String);
-
-impl FromStr for QuestionId {
-    type Err = Error;
-
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        match id.is_empty() {
-            false => Ok(QuestionId(id.to_string())),
-            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct InvalidId;
-
-impl Reject for InvalidId {}
 
 #[derive(Clone)]
 struct Store {
@@ -74,18 +42,20 @@ impl Store {
     }
 }
 
-async fn get_questions(store: Store) -> Result<impl warp::Reply, warp::Rejection> {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First question".to_string(),
-        "Content".to_string(),
-        Some(vec!["faq".to_string()]),
-    );
+async fn get_questions(
+    params: HashMap<String, String>,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut start = 0;
 
-    match question.id.0.parse::<i32>() {
-        Err(_) => Err(warp::reject::custom(InvalidId)),
-        Ok(_) => Ok(warp::reply::json(&question)),
-    }
+    if let Some(value) = params.get("start") {
+        start = value.parse::<usize>().expect("Could not parse data");
+    };
+
+    println!("{start}");
+
+    let questions: Vec<Question> = store.questions.values().cloned().collect();
+    Ok(warp::reply::json(&questions))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -93,11 +63,6 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
         Ok(warp::reply::with_status(
             error.to_string(),
             StatusCode::FORBIDDEN,
-        ))
-    } else if let Some(_invalid_id) = r.find::<InvalidId>() {
-        Ok(warp::reply::with_status(
-            "No valid ID presented".to_string(),
-            StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
@@ -120,6 +85,7 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
+        .and(warp::query())
         .and(store_filter)
         .and_then(get_questions)
         .recover(return_error);
