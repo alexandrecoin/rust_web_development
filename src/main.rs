be0ddd87bd611ba36ac7129e5b5d9775d913d487
Alexandre COIN <alexandre@macbook-pro-de-alexandre.home>
2024-05-29 05:53:09 +0200

    add insert question route

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
-use std::num::ParseIntError;
+use std::sync::Arc;
 use tokio;
+use tokio::sync::RwLock;
 use warp::reject::Reject;
 use warp::{
     cors::CorsForbidden,
@@ -23,18 +24,18 @@ struct QuestionId(String);
 
 #[derive(Clone)]
 struct Store {
-    questions: HashMap<QuestionId, Question>,
+    questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
 }
 
 impl Store {
     fn new() -> Self {
         Store {
-            questions: Self::init(),
+            questions: Arc::new(RwLock::new(Self::init())),
         }
     }
 
-    fn add_question(mut self, question: Question) -> Self {
-        self.questions.insert(question.id.clone(), question);
+    async fn add_question(mut self, question: Question) -> Self {
+        self.questions.write().await.insert(question.id.clone(), question);
         self
     }
 
@@ -49,14 +50,19 @@ async fn get_questions(
     store: Store,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     if !params.is_empty() {
-        let questions: Vec<Question> = store.questions.values().cloned().collect();
+        let questions: Vec<Question> = store.questions.read().await.values().cloned().collect();
         let pagination = extract_pagination(params, questions.len())?;
         let questions = &questions[pagination.start..pagination.end];
         Ok(warp::reply::json(&questions))
     } else {
-        let questions: Vec<Question> = store.questions.values().cloned().collect();
+        let questions: Vec<Question> = store.questions.read().await.values().cloned().collect();
         Ok(warp::reply::json(&questions))
     }
+}
+
+async fn add_question(store: Store, question: Question) -> Result<impl Reply, Rejection> {
+    store.questions.write().await.insert(question.id.clone(), question);
+    Ok(warp::reply::with_status("Question added", StatusCode::OK))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -93,8 +99,12 @@ impl std::fmt::Display for Error {
                 write!(f, "Cannot parse parameter: {}", err)
             }
             Error::MissingParameters => write!(f, "Missing one or more parameters"),
-            Error::NonProcessable => write!(f, "Start parameter cannot be greater than end parameter"),
-            Error::OutOfBounds => write!(f, "Not enough questions, please lower your end parameter"),
+            Error::NonProcessable => {
+                write!(f, "Start parameter cannot be greater than end parameter")
+            }
+            Error::OutOfBounds => {
+                write!(f, "Not enough questions, please lower your end parameter")
+            }
         }
     }
 }
@@ -107,10 +117,21 @@ struct Pagination {
     end: usize,
 }
 
-fn extract_pagination(params: HashMap<String, String>, max_possible_length: usize) -> Result<Pagination, Error> {
+fn extract_pagination(
+    params: HashMap<String, String>,
+    max_possible_length: usize,
+) -> Result<Pagination, Error> {
     if params.contains_key("start") && params.contains_key("end") {
-        let start = params.get("start").unwrap().parse::<usize>().map_err(Error::ParseError)?;
-        let end = params.get("end").unwrap().parse::<usize>().map_err(Error::ParseError)?;
+        let start = params
+            .get("start")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(Error::ParseError)?;
+        let end = params
+            .get("end")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(Error::ParseError)?;
 
         if start > end {
             return Err(Error::NonProcessable);
@@ -120,10 +141,7 @@ fn extract_pagination(params: HashMap<String, String>, max_possible_length: usiz
             return Err(Error::OutOfBounds);
         };
 
-        return Ok(Pagination {
-            start,
-            end,
-        })
+        return Ok(Pagination { start, end });
     }
     Err(Error::MissingParameters)
 }
@@ -142,11 +160,17 @@ async fn main() {
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and(warp::query())
-        .and(store_filter)
-        .and_then(get_questions)
-        .recover(return_error);
+        .and(store_filter.clone())
+        .and_then(get_questions);
 
-    let routes = get_items.with(cors);
+    let add_item = warp::post()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::json())
+        .and_then(add_question);
+
+    let routes = get_items.or(add_item).with(cors).recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
