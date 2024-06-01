b1c7cedbba18a227aa8ea52bdf70e6b511045199
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-06-01 13:10:41 +0200

    create answer handler

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,26 @@ struct Question {
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, Hash, PartialEq)]
 struct QuestionId(String);
 
+struct Answer {
+    id: AnswerId,
+    content: String,
+    question_id: QuestionId,
+}
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+struct AnswerId(String);
+
 #[derive(Clone)]
 struct Store {
     questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
+    answers: Arc<RwLock<HashMap<AnswerId, Answer>>>,
 }
 
 impl Store {
     fn new() -> Self {
         Store {
             questions: Arc::new(RwLock::new(Self::init())),
+            answers: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
@@ -89,7 +100,32 @@ async fn delete_question(id: String, store: Store) -> Result<impl Reply, Rejecti
     return match store.questions.write().await.remove(&QuestionId(id)) {
         Some(_) => Ok(warp::reply::with_status("Question deleted", StatusCode::OK)),
         None => Err(warp::reject::custom(Error::QuestionNotFound)),
-    }
+    };
+}
+
+async fn add_answer(
+    store: Store,
+    params: HashMap<String, String>,
+) -> Result<impl Reply, Rejection> {
+    let answer = Answer {
+        id: AnswerId("1".to_string()),
+        content: match params.get("content") {
+            Some(c) => c.to_string(),
+            None => return Err(warp::reject::custom(Error::MissingParameters)),
+        },
+        question_id: match params.get("questionId") {
+            Some(id) => QuestionId(id.to_string()),
+            None => return Err(warp::reject::custom(Error::MissingParameters)),
+        },
+    };
+
+    store
+        .answers
+        .write()
+        .await
+        .insert(answer.id.clone(), answer);
+
+    Ok(warp::reply::with_status("Answer created", StatusCode::OK))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -222,10 +258,18 @@ async fn main() {
         .and(store_filter.clone())
         .and_then(delete_question);
 
+    let add_answer = warp::post()
+        .and(warp::path("answers"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::form())
+        .and_then(add_answer);
+
     let routes = get_items
         .or(add_item)
         .or(update_item)
         .or(delete_item)
+        .or(add_answer)
         .with(cors)
         .recover(return_error);
 
